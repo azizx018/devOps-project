309c755da9c3b51dc8388aa1c16a82afee7d4740
Aziz, Chiara <chiara_aziz@optum.com>
2022-10-19 12:58:11 -0500

    added logging to add to cart

--- projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/CartController.java
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/CartController.java
@@ -3,6 +3,8 @@ package com.example.demo.controllers;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +24,7 @@ import com.example.demo.model.requests.ModifyCartRequest;
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
+	private static final Logger log = LoggerFactory.getLogger(CartController.class);
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -43,9 +46,12 @@ public class CartController {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
 		Cart cart = user.getCart();
+
+
 		IntStream.range(0, request.getQuantity())
 			.forEach(i -> cart.addItem(item.get()));
 		cartRepository.save(cart);
+		log.info("this is the cart ", cartRepository.save(cart));
 		return ResponseEntity.ok(cart);
 	}
 	
