6b1152bcf323a6bb68ac8ae0fb16edd2bcb22c4f
Aziz, Chiara <chiara_aziz@optum.com>
2022-10-20 13:57:44 -0500

    added statement in application root to get logging to work

--- projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/CartController.java
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/CartController.java
@@ -3,8 +3,10 @@ package com.example.demo.controllers;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import org.apache.logging.log4j.LogManager;
+
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +26,8 @@ import com.example.demo.model.requests.ModifyCartRequest;
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
-	private static final Logger log = LoggerFactory.getLogger(CartController.class);
+	private static final Logger log = LogManager.getLogger(CartController.class);
+	//private static final Logger log = LoggerFactory.getLogger(CartController.class);
 	
 	@Autowired
 	private UserRepository userRepository;
--- projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,9 +1,10 @@
 package com.example.demo.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +26,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	private static final Logger log = LogManager.getLogger(UserController.class);
+	//private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	
 	@Autowired
 	private UserRepository userRepository;
